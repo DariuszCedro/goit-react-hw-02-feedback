175ab2c3998c5c061b5fb7902057471edc35d6bd
DariuszCedro <dariuszcedro@gmail.com>
2024-01-24 17:20:04 +0100

    feature/statistic-counter: added page template with statistic counter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = evt => {
+    const { name } = evt.target;
+    this.setState(state => ({ [name]: state[name] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    const summary = good + neutral + bad;
+    if (summary === 0) {
+      return 0;
+    }
+    return Math.round((good / summary) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const percentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <button type="button" name="good" onClick={this.handleIncrement}>
+          Good
+        </button>
+        <button type="button" name="neutral" onClick={this.handleIncrement}>
+          Neutral
+        </button>
+        <button type="button" name="bad" onClick={this.handleIncrement}>
+          Bad
+        </button>
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Positive feedback: {percentage}%</p>
+      </div>
+    );
+  }
+}
